620f90ede93a5007da44523c9f8dd489e4aa1805
khila <badii.khila@gmail.com>
2024-06-08 13:46:35 +0200

    init project

---
+++ src/main/java/com/khila/nativedockerspring/NativeDockerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.khila.nativedockerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeDockerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeDockerSpringApplication.class, args);
+	}
+
+}
